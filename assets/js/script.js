9a48537ae1e693be5bee3dac69ca3b405d329ecf
_bogeyman_ <dewamah@gmail.com>
2017-05-21 18:03:59 +0800

    seccond commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,1 @@
-new WOW().init();
-$(document).ready(function(){
- $('.brand-logo').hide();
- $('.button-collapse').sideNav({closeOnClick: true});
- $('.parallax').parallax();
- $('.scrollspy').scrollSpy({scrollOffset:90});
- $('.slogan').typeIt({
-  strings: ["<h4>We are the FIRST IT Security in BALI...</h4>", "<h4>Let's Make Your System More Secure With Us...</h4>"],
-  speed: 100,
-  breakLines: false,
-  autoStart: true,
-  loop: true,
-  loopDelay: 1000,     
-  html: true
- });
-});
-
+(new WOW).init(),$(document).ready(function(){$(".brand-logo").hide(),$(".button-collapse").sideNav({closeOnClick:!0}),$(".parallax").parallax(),$(".scrollspy").scrollSpy({scrollOffset:90}),$(".slogan").typeIt({strings:["<h4>We are the FIRST IT Security in BALI...</h4>","<h4>Let's Make Your System More Secure With Us...</h4>"],speed:100,breakLines:!1,autoStart:!0,loop:!0,loopDelay:1e3,html:!0})});
